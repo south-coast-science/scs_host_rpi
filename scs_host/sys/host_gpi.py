7c46906989524da7eba691923ac0898a131e8def
Bruno Beloff <bruno.beloff@southcoastscience.com>
2017-05-12 12:04:29 +0100

    Added GPIO skeleton classes to sys package.

---
+++ scs_host/sys/host_gpi.py
@@ -0,0 +1,39 @@
+"""
+Created on 12 May 2017
+
+@author: Bruno Beloff (bruno.beloff@southcoastscience.com)
+"""
+
+from scs_host.sys.host_gpio import HostGPIO
+
+
+# --------------------------------------------------------------------------------------------------------------------
+
+# noinspection PyUnusedLocal,PyAbstractClass
+class HostGPI(HostGPIO):
+    """
+    classdocs
+    """
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def __init__(self, pin):
+        raise NotImplementedError
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    @property
+    def state(self):
+        raise NotImplementedError
+
+
+    def wait(self, edge):
+        raise NotImplementedError
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def __str__(self, *args, **kwargs):
+        raise NotImplementedError
+
---
+++ scs_host/sys/host_gpo.py
@@ -0,0 +1,39 @@
+"""
+Created on 12 May 2017
+
+@author: Bruno Beloff (bruno.beloff@southcoastscience.com)
+"""
+
+from scs_host.sys.host_gpio import HostGPIO
+
+
+# --------------------------------------------------------------------------------------------------------------------
+
+# noinspection PyUnusedLocal,PyAbstractClass
+class HostGPO(HostGPIO):
+    """
+    classdocs
+    """
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def __init__(self, pin, state):
+        raise NotImplementedError
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    @property
+    def state(self):
+        raise NotImplementedError
+
+
+    @state.setter
+    def state(self, state):
+        raise NotImplementedError
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def __str__(self, *args, **kwargs):
+        raise NotImplementedError
