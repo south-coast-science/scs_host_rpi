261935ed90280eed4a2025ff7619817c91626c1d
Bruno Beloff <bruno.beloff@southcoastscience.com>
2017-05-12 09:30:37 +0100

    Moved network package to scs_host.

---
+++ scs_host/network/__init__.py
@@ -0,0 +1,14 @@
+"""
+Created on 16 Apr 2017
+
+@author: Bruno Beloff (bruno.beloff@southcoastscience.com)
+"""
+
+from scs_host.network.wpa_supplicant_file import WPASupplicantFile
+
+
+# TODO: move the network package to host
+
+# --------------------------------------------------------------------------------------------------------------------
+
+WPASupplicantFile.init()
