7b2deefca317a022788ea7f892f995a14bc5c105
Bruno Beloff <bruno.beloff@southcoastscience.com>
2017-11-21 20:18:03 +0000

    Permitted HTTPClient to establish non-secure connections.

--- scs_host/client/http_client.py
+++ scs_host/client/http_client.py
@@ -29,8 +29,12 @@ class HTTPClient(object):
         self.__host = None
 
 
-    def connect(self, host):
-        self.__conn = http.client.HTTPSConnection(host)
+    def connect(self, host, secure=True):
+        if secure:
+            self.__conn = http.client.HTTPSConnection(host)
+        else:
+            self.__conn = http.client.HTTPConnection(host)
+
         self.__host = host
 
 
