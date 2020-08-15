f42562fbee887531d4ea663f3a18d9a264ae5eba
Bruno Beloff <bruno.beloff@southcoastscience.com>
2020-08-15 10:19:00 +0100

    Added UDS client and server classes

--- src/scs_host/comms/logging_domain_socket.py
+++ src/scs_host/comms/logging_domain_socket.py
@@ -5,12 +5,14 @@ Created on 26 May 2017
 
 A Unix domain socket abstraction, implementing ProcessComms
 
+Only one reader per UDS!
+
 https://pymotw.com/2/socket/uds.html
-https://gist.github.com/BenKnisley/5647884
 """
 
 import os
 import socket
+import sys
 import time
 
 from scs_core.sys.process_comms import ProcessComms
@@ -23,10 +25,10 @@ class DomainSocket(ProcessComms):
     classdocs
     """
 
-    EOM = '\n'                              # end of message for client-server communications
-
     __BACKLOG = 1                           # number of unaccepted connections before refusing new connections
     __BUFFER_SIZE = 1024
+
+    __WAIT_FOR_AVAILABILITY = 10.0          # seconds
 
     # ----------------------------------------------------------------------------------------------------------------
 
@@ -55,20 +57,24 @@ class DomainSocket(ProcessComms):
         self.__logger = logger              # Logger (for compatibility only)
 
         self.__socket = None                # socket.socket
-        self.__conn = None
 
 
     # ----------------------------------------------------------------------------------------------------------------
 
     def connect(self, wait_for_availability=True):
-        self.__socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        while True:
+            try:
+                self.__socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+                return
 
+            except ConnectionRefusedError as ex:
+                if not wait_for_availability:
+                    raise ex
 
-    def accept(self):
-        self.__socket.bind(self.path)
-        self.__socket.listen(self.__BACKLOG)
+                print("DomainSocket.connect: waiting for availability.", file=sys.stderr)
+                sys.stderr.flush()
 
-        self.__conn, _ = self.__socket.accept()
+                time.sleep(self.__WAIT_FOR_AVAILABILITY)
 
 
     def close(self):
@@ -77,80 +83,42 @@ class DomainSocket(ProcessComms):
 
 
     # ----------------------------------------------------------------------------------------------------------------
-    # client-server API...
-
-    def server_send(self, message):
-        self.__conn.send((message + self.EOM).encode())
-
-
-    def client_send(self, message):
-        try:
-            self.__socket.connect(self.path)
-        except OSError:
-            pass                    # assume that the socket is already connected
-
-        self.__socket.send((message + self.EOM).encode())
-
-
-    def server_receive(self):
-        return self.__receive(self.__conn)
-
-
-    def client_receive(self):
-        return self.__receive(self.__socket)
-
-
-    # ----------------------------------------------------------------------------------------------------------------
-
-    def __receive(self, channel):
-        message = ''
-        while True:
-            char = channel.recv(1).decode()
-
-            if char == self.EOM:
-                return message
-
-            if len(message) == self.__BUFFER_SIZE:
-                raise ValueError(message)
-
-            message += char
-
-
-    # ----------------------------------------------------------------------------------------------------------------
-    # unidirectional API...
 
     def read(self):                                             # blocking
         # socket...
-        self.__socket.bind(self.path)
+        self.__socket.bind(self.__path)
         self.__socket.listen(DomainSocket.__BACKLOG)
 
         try:
             while True:
-                self.__conn, _ = self.__socket.accept()
+                connection, _ = self.__socket.accept()
 
                 try:
                     # data...
-                    yield DomainSocket.__read(self.__conn).strip()
+                    yield DomainSocket.__read(connection).strip()
 
                 finally:
-                    self.__conn.close()
+                    connection.close()
 
         finally:
-            os.unlink(self.path)
+            os.unlink(self.__path)
 
 
     def write(self, message, wait_for_availability=True):       # message is dispatched on close()
         # socket...
         while True:
             try:
-                self.__socket.connect(self.path)
+                self.__socket.connect(self.__path)
                 break
 
             except (socket.error, FileNotFoundError) as ex:
-                if not wait_for_availability:
-                    raise ConnectionRefusedError(ex)
+                print("DomainSocket.write: %s" % ex, file=sys.stderr)
+                sys.stderr.flush()
 
-                time.sleep(0.1)
+                if not wait_for_availability:
+                    raise ex
+
+                time.sleep(self.__WAIT_FOR_AVAILABILITY)
 
         # data...
         self.__socket.sendall(message.strip().encode())
