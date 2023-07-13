0f5701c628c0f5b6ee08491b10beba1fec9dfd5b
Bruno Beloff <bruno.beloff@southcoastscience.com>
2023-07-13 11:40:14 +0100

    Compatibility updates

--- src/scs_host/sys/host.py
+++ src/scs_host/sys/host.py
@@ -64,6 +64,8 @@ class Host(IoTNode, FilesystemPersistenceManager):
     # ----------------------------------------------------------------------------------------------------------------
     # directories and files...
 
+    __HOSTNAME_PREFIX =     'scs-rpi-'
+
     __DEFAULT_HOME_DIR =    "/home/pi"                          # hard-coded abs path
     __LOCK_DIR =            "/run/lock/southcoastscience"       # hard-coded abs path
     __TMP_DIR =             "/tmp/southcoastscience"            # hard-coded abs path
@@ -118,6 +120,11 @@ class Host(IoTNode, FilesystemPersistenceManager):
 
     # ----------------------------------------------------------------------------------------------------------------
     # network identity...
+
+    @classmethod
+    def hostname_prefix(cls):
+        return cls.__HOSTNAME_PREFIX
+
 
     @classmethod
     def name(cls):
